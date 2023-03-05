d0878a93909e9d9b5d1b997ef47ff6ecc88b7076
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-03-05 21:10:48 +0000

    windows test file

---
+++ run.ps1
@@ -0,0 +1,6 @@
+cargo build --release
+
+$env:DISCORD_TOKEN = 'Bot MTA3OTEwNDg0MTc4MTEwNDcwMA.GkZrF5.R_ZgbaCDd0k-OcWA1mEpOtAhWv2_M4aqPHPL6A'
+$env:PORT = '3000'
+
+./target/release/twilight-http-proxy

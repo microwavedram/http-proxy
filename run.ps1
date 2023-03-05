6bbaa8e38d6871a29c6547f7fb231bd7a0e33d3e
microwavedram <89029844+microwavedram@users.noreply.github.com>
2023-03-05 21:14:06 +0000

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 cargo build --release
 
-$env:DISCORD_TOKEN = 'Bot MTA3OTEwNDg0MTc4MTEwNDcwMA.GkZrF5.R_ZgbaCDd0k-OcWA1mEpOtAhWv2_M4aqPHPL6A'
+$env:DISCORD_TOKEN = ''
 $env:PORT = '3000'
 
 ./target/release/twilight-http-proxy
